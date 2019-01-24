9d5f6f78941d6ffe8eb7b9e85935339161c8741e
Muhammad Usman <muhammadusman1.me@gmail.com>
2019-01-24 07:43:25 +0500

    Added evaluation measures

--- utils/generator.py
+++ utils/generator.py
@@ -25,14 +25,18 @@ def generate(config):
         max_ep_steps=config['max_episode_steps']
         done=True
         gain=config['gain']
+        eps=config['eps']
         ite=0
         ite2 = 0
         log_dir = 'tf_logs'
         reward_placeholder = tf.placeholder(tf.float32, shape=(), name='Reward')
+        total_q_placeholder=tf.placeholder(tf.float32, shape=(), name='Q')
         reward_summary = tf.summary.scalar('Reward', reward_placeholder)
+        q_summary = tf.summary.scalar('Reward', total_q_placeholder)
         file_writer=tf.summary.FileWriter(log_dir, tf.get_default_graph())
         episode_reward=0
         episode=0
+        total_q=0
         print ('Loop Started')
         while True:
             with saved_new.get_lock():
@@ -40,20 +44,28 @@ def generate(config):
                     config['policy'].load_weights()
                     print ("New Network Loaded")
                     saved_new.value=0
+                    ite += 1
+                    gain *= 1 / (1 + decay * ite)
+                    eps *= 1 / (1 + decay * ite)
             if done or ite2>max_ep_steps:
-                print('Steps Completed in current episode:{} wirh reward {}'.format(ite2,episode_reward))
-                ite2=0
+                print('Steps Completed in current episode:{} with reward {} and q {} and mean r'.format(ite2,episode_reward,total_q,episode_reward/ite2,total_q/ite2))
                 obs=env.reset()
                 obs=FP.process_frame(obs)
                 reward=0
                 with tf.Session() as sess:
                     summary_str = reward_summary.eval(feed_dict={reward_placeholder:episode_reward})
                     file_writer.add_summary(summary_str, episode)
+                    summary_str = reward_summary.eval(feed_dict={reward_placeholder: total_q/ite2})
+                    file_writer.add_summary(summary_str, episode)
                 for i in range(config['nstack']):
                     obs_queue.append(obs)
                 episode_reward = 0
                 episode+=1
-            action = explore_boltzman(obs_queue, gain, config['policy'], actions)
+                ite2 = 0
+            if config['exploration']=='greedy':
+                action = explore_eps_greedy(obs_queue, eps, config['policy'], actions)
+            elif config['exploration']=='boltzman':
+                action = explore_boltzman(obs_queue, gain, config['policy'], actions)
             observation, reward, done, info=env.step(action)
             episode_reward+=reward
             obs = FP.process_frame(observation)
@@ -62,7 +74,6 @@ def generate(config):
             observation = observation.reshape(1, *observation.shape)
             q_values = config['policy'].Model.predict(observation)[0]
             true_reward=reward+gamma*np.max(q_values)
+            total_q+=true_reward
             buffer.put((observation,true_reward,action,done),block=True,timeout=None)
-            gain *= 1 / (1 + decay * ite)
             ite2+=1
-            ite+=1
--- utils/generator_utils.py
+++ utils/generator_utils.py
@@ -21,6 +21,6 @@ def explore_eps_greedy(observation,eps,Policy_Network,actions):
     observation = observation.reshape(1, *observation.shape)
     q_values = Policy_Network.Model.predict(observation)[0]
     ind=np.argmax(q_values)
-    prob=np.ones(actions.shape)*eps
+    prob=(np.ones(actions.shape)*eps)/(actions.shape[0]-1)
     prob[ind]=1-eps
     return np.random.choice(actions, p=prob)
